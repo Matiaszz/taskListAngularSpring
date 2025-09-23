b93c06265ca49379a16953861fc372444b83336d
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-09-23 17:01:18 -0300

    feat: login style

--- tasklist-frontend/src/app/app.ts
+++ tasklist-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Auth } from './components/auth/auth';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ tasklist-frontend/src/app/components/navbar/navbar.ts
@@ -0,0 +1,10 @@
+import { Component, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+}
